35a8ce40fd212988b0eb52b6cb242f2bf89453d2
hmneto <hmneton@gmail.com>
2022-11-02 20:43:21 -0300

    update

---
+++ src/test/java/br/com/app/Case.java
@@ -0,0 +1,22 @@
+package br.com.app;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class Case {
+
+
+		  public void testaRegistro() throws Exception {
+		      Assertions.assertEquals(10,10);  
+		  }
+
+
+}
