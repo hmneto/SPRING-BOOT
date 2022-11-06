55312db13ee9d20b6a032f78127e4f3b3afd271d
hmneto <hmneton@gmail.com>
2022-11-06 19:41:15 -0300

    cors test

--- src/main/java/br/com/app/controller/WebConfig.java
+++ src/main/java/br/com/app/controller/WebConfig.java
@@ -1,11 +1,13 @@
-package br.com.app.config.security;
+package br.com.app.controller;
 
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class WebConfig implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
