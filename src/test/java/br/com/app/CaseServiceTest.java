7be34fbfc5202c41ed3e4f74297db5662a79e131
hmneto <hmneton@gmail.com>
2022-11-05 21:29:16 -0300

    Update CaseServiceTest.java

--- src/test/java/br/com/app/CaseServiceTest.java
+++ src/test/java/br/com/app/CaseServiceTest.java
@@ -1,16 +1,16 @@
 package br.com.app;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.runner.RunWith;
-import org.junit.jupiter.api.Test;
+// import org.junit.runner.RunWith;
+// import org.junit.jupiter.api.Test;
 
 
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-import org.springframework.test.context.junit4.SpringRunner;
+// import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+// @RunWith(SpringRunner.class)
 @SpringBootTest
 class CaseServiceTest {
 
