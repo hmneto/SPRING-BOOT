18ea6198fe8166e95f75a8fd4fc2f14db555be88
hmneto <hmneton@gmail.com>
2022-11-05 23:07:08 -0300

    Update Application.java

--- src/main/java/br/com/app/Application.java
+++ src/main/java/br/com/app/Application.java
@@ -48,24 +48,24 @@ public class Application {
 	@Transactional
 	@GetMapping("/")
 	public String Index() {
-		String senha = "$2a$10$xQKue46QQjssCHNZMUWCw.JpOwocN733j8Gr7txYDD5zhfGune/S.";
-		RoleModel roleAdmin = new RoleModel(RoleName.ROLE_ADMIN);
-		roleRepository.save(roleAdmin);
+		// String senha = "$2a$10$xQKue46QQjssCHNZMUWCw.JpOwocN733j8Gr7txYDD5zhfGune/S.";
+		// RoleModel roleAdmin = new RoleModel(RoleName.ROLE_ADMIN);
+		// roleRepository.save(roleAdmin);
 
-		RoleModel roleUser = new RoleModel(RoleName.ROLE_USER);
-		roleRepository.save(roleUser);
+		// RoleModel roleUser = new RoleModel(RoleName.ROLE_USER);
+		// roleRepository.save(roleUser);
 
-		List<RoleModel> rolesAdmin = new ArrayList<RoleModel>();
-		rolesAdmin.add(roleAdmin);
+		// List<RoleModel> rolesAdmin = new ArrayList<RoleModel>();
+		// rolesAdmin.add(roleAdmin);
 
-		UserModel Admin = new UserModel("admin", senha, rolesAdmin);
-		usuarioRepository.save(Admin);
+		// UserModel Admin = new UserModel("admin", senha, rolesAdmin);
+		// usuarioRepository.save(Admin);
 
-		List<RoleModel> rolesUser = new ArrayList<RoleModel>();
-		rolesUser.add(roleUser);
+		// List<RoleModel> rolesUser = new ArrayList<RoleModel>();
+		// rolesUser.add(roleUser);
 
-		UserModel User = new UserModel("user", senha, rolesUser);
-		usuarioRepository.save(User);
+		// UserModel User = new UserModel("user", senha, rolesUser);
+		// usuarioRepository.save(User);
 
 		return "index";
 	}
