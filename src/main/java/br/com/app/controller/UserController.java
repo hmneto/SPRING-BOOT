00fee5e2aa2cc2a67377e25edb6796ed401b28de
hmneto <hmneton@gmail.com>
2022-11-06 17:02:22 -0300

    Update UserController.java

--- src/main/java/br/com/app/controller/UserController.java
+++ src/main/java/br/com/app/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class UserController {
 	@GetMapping
+	@CrossOrigin
 	public String index() {
 		return "User";
 	}
