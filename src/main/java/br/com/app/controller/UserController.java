0dd0173589153581ca0e242ddb11886c1232d50b
hmneto <hmneton@gmail.com>
2022-11-06 16:59:38 -0300

    Update UserController.java

--- src/main/java/br/com/app/controller/UserController.java
+++ src/main/java/br/com/app/controller/UserController.java
@@ -1,11 +1,13 @@
 package br.com.app.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin
 public class UserController {
 	@GetMapping
 	public String index() {
