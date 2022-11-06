dfe321f33eb8ffea2300845dad1bd2e046fb38f6
hmneto <hmneton@gmail.com>
2022-11-06 20:12:43 -0300

    Update WebSecurityConfig.java

--- src/main/java/br/com/app/config/security/WebSecurityConfig.java
+++ src/main/java/br/com/app/config/security/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package br.com.app.config.security;
 
 import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,6 +9,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 //@Configuration
+@SuppressWarnings("deprecation")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	public UserDetailsServiceImpl userDetailsService;
