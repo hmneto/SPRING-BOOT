6ad6bde8fe67bb38274f43005689367379202e51
hmneto <hmneton@gmail.com>
2022-11-12 14:47:23 -0300

    Update CaseController.java

--- src/main/java/br/com/app/controller/CaseController.java
+++ src/main/java/br/com/app/controller/CaseController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.app.model.CaseModel;
@@ -16,11 +17,13 @@ import br.com.app.model.CaseModel;
 public class CaseController {
 
 	@GetMapping
+	@ResponseBody
 	public String getAll() {
 		return "OK";
 	}
 	
 	@PostMapping
+	@ResponseBody
 	public ResponseEntity<CaseModel> save(CaseModel caseModel){
 		return ResponseEntity.status(HttpStatus.OK).body(caseModel);
 	}
