7375ca6eccdad2406de0de66bea2b6866d34dca2
hmneto <hmneton@gmail.com>
2022-11-12 23:37:53 -0300

    Create EmailModel.java

---
+++ src/main/java/br/com/app/model/EmailModel.java
@@ -0,0 +1,31 @@
+package br.com.app.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "TB_EMAIL")
+public class EmailModel {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer emailId;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDateEmail;
+    //private StatusEmail statusEmail;
+}
